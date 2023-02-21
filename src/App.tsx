ff7862de1a4853307dd4a037b0141c02a3e37407
Faris Durrani <fdurrani@mythics.com>
2023-02-21 11:12:12 -0500

    Added modal to navbarmain

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,33 @@
 import "./assets/styles/App.scss";
 import "react-toastify/dist/ReactToastify.css";
-import FocusLock from "react-focus-lock";
 import Footer from "./features/Footer";
 import Home from "./pages/Home";
 import NavbarMain from "./features/navbar-menu/NavbarMain";
 import React, { useState, useRef } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { ToastContainer } from "react-toastify";
-import { useOnClickOutside } from "./hooks/useOnClickOutside";
 import { EditSurvey, SurveyResponses, Surveys } from "./pages/surveys";
-// import Burger from "./features/navbar-menu/Burger";
-// import Menu from "./features/navbar-menu/Menu";
+import { ToastContainer } from "react-toastify";
+import { Modal } from "react-bootstrap";
 
 const App = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const node = useRef<HTMLDivElement>(null);
-  const menuId = "main-menu";
-
-  useOnClickOutside(node, () => setMenuOpen(false));
-
   return (
     <div id="app">
-      <ThemeProvider theme={{}}>
-        <BrowserRouter>
-          <div id="app-router">
-            <NavbarMain />
-            <Routes>
-              <Route path="/" element={<Navigate to="/surveys" />} />
-              <Route path="/surveys" element={<Surveys />} />
-              <Route path="/surveys/edit-survey" element={<EditSurvey />} />
-              <Route
-                path="/surveys/survey-responses"
-                element={<SurveyResponses />}
-              />
-            </Routes>
-            <ToastContainer autoClose={2500} />
-            <Footer />
-            {/* <div ref={node}>
-              <FocusLock disabled={!menuOpen}>
-                <Burger
-                  menuOpen={menuOpen}
-                  setMenuOpen={setMenuOpen}
-                  aria-controls={menuId}
-                />
-                <Menu
-                  menuOpen={menuOpen}
-                />
-              </FocusLock>
-            </div> */}
-          </div>
-        </BrowserRouter>
-      </ThemeProvider>
+      <BrowserRouter>
+        <div id="app-router">
+          <NavbarMain />
+          <Routes>
+            <Route path="/" element={<Navigate to="/surveys" />} />
+            <Route path="/surveys" element={<Surveys />} />
+            <Route path="/surveys/edit-survey" element={<EditSurvey />} />
+            <Route
+              path="/surveys/survey-responses"
+              element={<SurveyResponses />}
+            />
+          </Routes>
+          <ToastContainer autoClose={2500} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
--- src/features/navbar-menu/NavbarMain.tsx
+++ src/features/navbar-menu/NavbarMain.tsx
@@ -1,26 +1,50 @@
-import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Nav, Navbar, NavDropdown, Modal } from "react-bootstrap";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const NavbarMain = () => {
+  const [show, setShow] = useState(false);
+  const MenuModal = (
+    <Modal
+      show={show}
+      onHide={() => setShow(false)}
+      dialogClassName="modal-left"
+      aria-labelledby="example-custom-modal-styling-title"
+    >
+      <Modal.Body>Being implemented...</Modal.Body>
+    </Modal>
+  );
+
   return (
-    <Navbar expand="lg" className="navbar">
-      <Container fluid>
-        <Navbar.Brand href="/" className="ms-5">
-          MYTHICS Automation Platform
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto me-0">
-            <NavDropdown title={<AccountCircleIcon />} id="basic-nav-dropdown">
-              <NavDropdown.Item href="#">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#">Settings</NavDropdown.Item>
-              <NavDropdown.Item href="#">Log out</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="navbar">
+        <Container fluid>
+          <button
+            className="navbar-menu-toggler me-2"
+            type="button"
+            onClick={() => setShow(true)}
+          >
+            <MenuIcon />
+          </button>
+          <Navbar.Brand href="/">MYTHICS Automation Platform</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto me-0">
+              <NavDropdown
+                title={<AccountCircleIcon />}
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item href="#">Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#">Settings</NavDropdown.Item>
+                <NavDropdown.Item href="#">Log out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      {MenuModal}
+    </>
   );
 };
 
