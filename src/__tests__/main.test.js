bfb250cb2d8f803cea9e426f9a3bfb8931ffa796
Faris Durrani <fdurrani@mythics.com>
2023-02-21 14:16:06 -0500

    dnd-kit

--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -1,12 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import { SurveyResponses } from "../pages/surveys";
 
 test("test", () => {
   expect(1).toBe(1);
 });
 
-test("renders Surveys page", () => {
+test("renders App page", () => {
   render(<App />);
   const title = screen.getAllByText(/Surveys/i);
+  expect(title[0]).toBeInTheDocument();
+});
+
+test("renders Surveys Responses page", () => {
+  render(<SurveyResponses />);
+  const title = screen.getAllByText(/Survey Responses/i);
   expect(title[0]).toBeInTheDocument();
 });
